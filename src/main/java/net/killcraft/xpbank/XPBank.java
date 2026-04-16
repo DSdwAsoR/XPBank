961868fb339bc017a9b9e80dba8fa0ccb9b5acfa
DSdwAsoR <vincelpinsel@gmail.com>
2026-04-16 19:00:58 +0200

    Update XPBank.java

--- src/main/java/net/killcraft/xpbank/XPBank.java
+++ src/main/java/net/killcraft/xpbank/XPBank.java
@@ -3,8 +3,19 @@ package net.killcraft.xpbank;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class XPBank extends JavaPlugin {
+
     @Override
     public void onEnable() {
-        getLogger().info("XPBank wurde geladen!");
+        // Diese Nachricht siehst du beim Starten in der Konsole
+        getLogger().info("=======================================");
+        getLogger().info("XPBank System fuer KillCraft geladen!");
+        getLogger().info("Status: Einsatzbereit (v1.0)");
+        getLogger().info("=======================================");
+    }
+
+    @Override
+    public void onDisable() {
+        // Diese Nachricht siehst du beim Stoppen des Servers
+        getLogger().info("XPBank wurde erfolgreich deaktiviert.");
     }
 }
