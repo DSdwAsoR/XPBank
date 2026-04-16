276c3b89f8c02a99a7dc20cd7a2d7b273bf113d4
DSdwAsoR <vincelpinsel@gmail.com>
2026-04-16 18:39:08 +0200

    Create XPBank.java

---
+++ src/main/java/net/killcraft/xpbank/XPBank.java
@@ -0,0 +1,10 @@
+package net.killcraft.xpbank;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class XPBank extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("XPBank wurde geladen!");
+    }
+}
